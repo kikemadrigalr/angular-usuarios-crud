dfedd6987dc22e1cdcefa0149f996a3a486cab07
CarlosMadrigal <carlos.madrigal@andinas.com>
2019-10-01 13:18:57 -0400

    validacion en conexion a bd

--- server/src/database.ts
+++ server/src/database.ts
@@ -6,9 +6,28 @@ const pool = mysql.createPool(keys.database);
 
 
 pool.getConnection((err, connection) => {
-    if (err) throw err;
-    connection.release();
-    console.log('Conectado a Base de Datos Usuarios');
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+          //no se establecio la conexion
+          console.error('La Conexión ha sido cerrada');
+        }
+    
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+          //cuantas conexiones tiene la base de datos
+          console.error('La base de datos tiene mas de una conexión')
+        }
+    
+        if (err.code === 'ECONNREFUSED') {
+          //la conexion ha sido rechazada
+          console.error('La conexión a la base de datos fue rechazada')
+        }
+      }
+
+      if(connection){
+        connection.release();
+        console.log('Se establecio la conexion a BD Usuarios');
+        return;
+      }
 });
 
 export default pool;
